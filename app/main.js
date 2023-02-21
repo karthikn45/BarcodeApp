84426dc20d732d3ca925a6af1b38c2d47003ffab
karthikn45 <72658594+karthikn45@users.noreply.github.com>
2023-02-21 16:59:15 +0530

    Update main.js

--- app/main.js
+++ app/main.js
@@ -4,13 +4,13 @@ barcodeApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider 
   .state("home", {
     url: "/",
-    templateUrl : "https://github.com/karthikn45/BarcodeApp/blob/view/generatebarcode.html",
+    templateUrl : "../view/generatebarcode.html",
     controller : "barCodeCtrl"
   })
 
   .state("trackingEntryForm", {
     url: "/trackingEntryForm/:id/:edit",
-    templateUrl : "https://github.com/karthikn45/BarcodeApp/blob/view/trackingEntryForm.html",
+    templateUrl : "../view/trackingEntryForm.html",
     controller : "trackingEntryCtrl"
   })
 
